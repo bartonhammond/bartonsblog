afdc91a1aaf01350361e462503c448d210082903
Barton Hammond <bartonhammond@gmail.com>
2015-05-26 18:54:23 -0500

    Blogs collection

--- client/public/blogs/blogs.js
+++ client/public/blogs/blogs.js
@@ -1,24 +1,7 @@
-var blogsData = [
-  {
-    typeCard: "film art",
-    img: "img/sample/sintel/sample-sintel-1.jpg",
-    title: "Sintel at Seattle International Film Festival",
-    written: "Saturday 27 April 2015",
-    description: "This 15 minute film has been realised in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realised online, by developers and artists and teams all over the world.",
-    filters: "Film",
-    numberComments: "3"
-  },
-  {
-    typeCard: "film art",
-    img: "img/sample/sintel/sample-sintel-1.jpg",
-    title: "Sintel at Seattle International Film Festival",
-    written: "Saturday 27 April 2015",
-    description: "This 15 minute film has been realised in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realised online, by developers and artists and teams all over the world.",
-    filters: "Film",
-    numberComments: "5"
-  }
-];
+Meteor.subscribe('blogs');
 
 Template.blogs.helpers({
-  blogs: blogsData
+  blogs: function(){
+    return Blogs.find();
+  }
 })
